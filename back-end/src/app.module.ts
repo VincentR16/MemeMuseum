7845554f7b546b2a9cc73828be06871da75db085
Vincenzo <vincy22.vr@gmail.com>
2025-10-16 13:12:19 +0200

    Implemented packages for the project:     -Installed and Implemented Throttler     -Installed and Implemented TypeOrm     -Installed and Implemented ClassSerielized

--- back-end/src/app.module.ts
+++ back-end/src/app.module.ts
@@ -1,10 +1,40 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { configValifationSchema } from './config.schema';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ThrottlerModule } from '@nestjs/throttler';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: ['.env'],
+      validationSchema: configValifationSchema,
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        autoLoadEntities: true,
+        synchronize: true,
+        host: configService.get('DB_HOST'),
+        port: configService.get('DB_PORT'),
+        username: configService.get('DB_USERNAME'),
+        password: configService.get('DB_PASSWORD'),
+        database: configService.get('DB_DATABASE'),
+      }),
+    }),
+    ThrottlerModule.forRoot({
+      throttlers: [
+        {
+          ttl: 30000,
+          limit: 5,
+        },
+      ],
+    }),
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
--- back-end/src/main.ts
+++ back-end/src/main.ts
@@ -1,9 +1,27 @@
-import { NestFactory } from '@nestjs/core';
+import { NestFactory, Reflector } from '@nestjs/core';
 import { AppModule } from './app.module';
+import {
+  ClassSerializerInterceptor,
+  Logger,
+  ValidationPipe,
+} from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const logger = new Logger();
   await app.listen(process.env.PORT ?? 3000);
-  console.log('okok');
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
+
+  app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
+
+  logger.log(`Application listening on port ${process.env.PORT ?? 3000}`);
 }
-bootstrap();
+void bootstrap();
