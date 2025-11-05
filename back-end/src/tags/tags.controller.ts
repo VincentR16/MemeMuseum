0003482f301f1089415c7a92b6dd342b240fd132
Vincenzo <vincy22.vr@gmail.com>
2025-11-05 17:27:04 +0100

    piccolo fix a tags

--- back-end/src/tags/tags.controller.ts
+++ back-end/src/tags/tags.controller.ts
@@ -11,21 +11,23 @@ import { JwtAuthGuard } from 'src/common/guard/jwt-auth.guard';
 import { Tag } from './tag.entity';
 
 @Controller('tag')
-@UseGuards(JwtAuthGuard)
 export class TagController {
   constructor(private readonly tagService: TagService) {}
 
   @Get('by-count')
+  @UseGuards(JwtAuthGuard)
   getByCount(): Promise<string[]> {
     return this.tagService.getByCount();
   }
 
   @Get('by-creation')
+  @UseGuards(JwtAuthGuard)
   getByCreation(): Promise<Tag[]> {
     return this.tagService.getByCreation();
   }
 
   @Delete(':name')
+  @UseGuards(JwtAuthGuard)
   async delete(@Param('name') name: string) {
     await this.tagService.delete(name);
   }
